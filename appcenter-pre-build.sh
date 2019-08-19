de025bb900586764636757290481cdb3acb3af30
Douglas Freitas <douglasofreitas@gmail.com>
2019-08-19 10:13:16 -0300

    APPCenter pre build configu files

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -6,3 +6,27 @@ printf "%s\n" $ENV_WHITELIST
 set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
 printf "\n.env created with contents:\n\n"
 cat .env
+
+#Config Android config files
+GOOGLE_CONFIG_ANDROID_FILE=$APPCENTER_SOURCE_DIRECTORY/android/app/google-services.json
+if [ -e "$GOOGLE_CONFIG_ANDROID_FILE" ]
+then
+    echo "Updating Google Json"
+    echo "$GOOGLE_CONFIG_ANDROID" > $GOOGLE_CONFIG_ANDROID_FILE
+    sed -i -e 's/\\"/'\"'/g' $GOOGLE_CONFIG_ANDROID_FILE
+
+    echo "File content:"
+    cat $GOOGLE_CONFIG_ANDROID_FILE
+fi
+
+#Config Android config files
+GOOGLE_CONFIG_IOS_FILE=$APPCENTER_SOURCE_DIRECTORY/ios/GoogleService-Info.plist
+if [ -e "$GOOGLE_CONFIG_IOS_FILE" ]
+then
+    echo "Updating Google Json"
+    echo "$GOOGLE_CONFIG_IOS" > $GOOGLE_CONFIG_IOS_FILE
+    sed -i -e 's/\\"/'\"'/g' $GOOGLE_CONFIG_IOS_FILE
+
+    echo "File content:"
+    cat $GOOGLE_CONFIG_IOS_FILE
+fi
